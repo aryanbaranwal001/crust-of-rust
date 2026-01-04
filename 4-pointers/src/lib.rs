608d0ad6356e443f6f0d407b33fafcb6c143e340
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-01-05 00:10:43 +0530

    learn: smart pointers and interior mutability

--- 4-pointers/src/lib.rs
+++ 4-pointers/src/lib.rs
@@ -2,3 +2,9 @@ mod cell;
 mod rc;
 mod refcell;
 // cell is used for smaller values, like numbers or flags
+
+// CoW smart pointer
+
+// cell => interior mutability
+// refcell => borrow checker at runtime with interior mutability
+// Rc => reference poitners at runtime, deallocation when count = 0,
